bad45ce97b0ebd3c8fabf364593aadeb70601a81
Risingvirtue <risingvirtue@berkeley.edu>
2019-01-28 20:57:02 -0800

    redux init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,40 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import Title from './Title/Title';
 import Score from './Score/Score';
 
+import { combineReducers, createStore } from 'redux';
+
+
+function userReducer(state = [], action) {
+  if (action.type == 'changeState') {
+    return action.payload.newState;
+  }
+  return state;
+}
+
+function wordReducer(state = {}, action) {
+  return state;
+}
+
+const allReducers = combineReducers({
+  words: wordReducer,
+  users: userReducer
+})
+const store = createStore(allReducers, {
+  words: {'test': true, 'why': true},
+  users: ['Johnny']
+});
+console.log(store.getState());
+
+const action = {
+  type:'changeState',
+  payload: {
+    newState: 'New State'
+  }
+
+}
+
+store.dispatch(action);
+
 class App extends React.Component {
 
   render() {
